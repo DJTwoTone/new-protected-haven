f1772b885a08739d430dae113e1a7f96bd5f2f4d
DJTwoTone <34737313+DJTwoTone@users.noreply.github.com>
2022-03-29 20:04:36 +0900

    working on home andd language features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
-import { Header, Footer, Logo } from './components'
+import {Route, Routes} from 'react-router-dom';
+import { Header, Footer, Logo, Home } from './components';
+import translations from './assets/translations';
 
 function App() {
+  
+  //get default browser language
+  //set that part of the translation to state
+  //if no translation set english  
+  console.log(Object.values(translations).map(lang => lang.name));
+  console.log(navigator.language)
+  
+  //pass translations to form from state
+
   return (
     <div className="App">
       <Header />
       <Logo />
-      {/* <Switch>
-
-
-      </Switch> */}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+
+    //on language select, check if it is curent, if not, change it
+  return (
+    <section>
+        <h2>Welcome. Please select you langauge</h2>
+        <div id="langauge-container">
+
+        </div>
+    </section>
+  )
+}
+
+export default Home
